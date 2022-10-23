74ba4d8c2fdc44824bbc00fb70f40d8df80e3e31
Ghoul12342 <114170053+Ghoul12342@users.noreply.github.com>
2022-10-23 19:19:46 +0300

    Add files via upload

---
+++ xczbscbvx.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//	Series1◦
+		//	.Даны десять вещественных чисел.Найти их сумму.
+	/*	float sum = 0, i = 1;
+		float x;
+		while (i <= 5) {
+			cin >> x;
+			sum += x;
+			i ++ ;
+		}
+		cout << sum;*/
+		//Series2.Даны десять вещественных чисел.Найти их произведение.
+		/*float prois = 1;
+		float x;
+		for (int i = 1; i <= 10; i++) {
+			cin >> x;
+			prois = prois * x;
+
+		}
+		cout << prois;*/
+		//Series3.Даны десять вещественных чисел.Найти их среднее арифметическое
+		/*float prois = 1;
+		float x;
+		int i = 1;
+		while (i <= 10) {
+			cin >> x;
+			prois = prois + x;
+			i++;
+
+		}
+		prois = prois / 10;
+		cout << prois;*/
+
+		/*Series4.Дано целое число N и набор из N вещественных чисел.Вывести
+			сумму и произведение чисел из данного набора.*/
+			/*int sum = 0;
+			int prois = 1;
+			int n;
+			cin >> n;
+			int i = 1;
+			while (i <= n) {
+				prois = prois * i;
+				sum = sum + i;
+				i++;
+			}
+			cout << sum << " " << prois;*/
+			/*Series5.Дано целое число N и набор из N положительных вещественных
+				чисел.Вывести в том же порядке целые части всех чисел из данного
+				набора(как вещественные числа с нулевой дробной частью), а также
+				сумму всех целых частей.*/
+
+/*	float  a;
+	int  n, sum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		
+		cin >> a;
+		cout << (int)a<<" ";
+		sum =sum+a;
+	}
+	cout  << sum<<" ";
+	
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
